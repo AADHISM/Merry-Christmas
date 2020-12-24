fc1bae77332b206f190317db5dc07680e47e7e90
Aadhithiya <66451374+AADHISM@users.noreply.github.com>
2020-12-24 16:34:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function gift1() {
+  alert("Peace on earth will come to stay, When we live Christmas every day.");
+}
+
+function gift2() {
+  alert(
+    "The best way to spread Christmas cheer is singing loud for all to hear."
+  );
+}
+
+function gift3() {
+  alert(
+    "Christmas waves a magic wand over this world, and behold, everything is softer and more beautiful."
+  );
+}
